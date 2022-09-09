d303d5a0e54b54e23b4f38d496cc0903150d44cf
Arlind Ramadani <arlindramadani34@gmail.com>
2022-09-10 00:24:24 +0200

    Implemented AntD and added the fonts and variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux';
+
+import 'antd/dist/antd.min.css';
+import './sass/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Header } from '../components/header/Header';
 import { setTheme } from '../redux/actions';
+import { Button } from 'antd';
 
 export const HomePage = () => {
   const colorScheme = useSelector((state) => state);
@@ -11,7 +12,7 @@ export const HomePage = () => {
     <>
       <Header />
       Color Theme: {colorScheme}
-      <button onClick={() => dispatch(setTheme(colorScheme))}>Switch theme</button>
+      <Button onClick={() => dispatch(setTheme(colorScheme))}>Switch theme</Button>
     </>
   );
 };
